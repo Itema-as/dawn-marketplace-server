c84070ec42795dd89e7cc15a9b649fbd6a55a720
Torkild U. Resheim <torkildr@gmail.com>
2016-02-16 13:58:47 +0100

    Fix #20: Use parameter to trigger initialisation of the marketplace
    Task-Url: https://github.com/Itema-as/eclipse-marketplace-server/issues/issue/20

---
+++ org.dawnsci.marketplace.server/src/main/resources/schema.sql
@@ -0,0 +1,22 @@
+create table if not exists UserConnection (userId varchar(255) not null,
+	providerId varchar(255) not null,
+	providerUserId varchar(255),
+	rank int not null,
+	displayName varchar(255),
+	profileUrl varchar(512),
+	imageUrl varchar(512),
+	accessToken varchar(512) not null,
+	secret varchar(512),
+	refreshToken varchar(512),
+	expireTime bigint,
+	primary key (userId, providerId, providerUserId));
+
+
+drop index if exists UserConnectionRank
+create unique index UserConnectionRank on UserConnection(userId, providerId, rank);
+
+create table if not exists Account (id identity,
+	username varchar(256) unique,
+	password varchar(256) not null,
+	firstName varchar(256) not null, 
+	lastName varchar(256) not null);
